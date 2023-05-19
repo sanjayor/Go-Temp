71e70040b087f16b45890128d30b11b8bc4aab12
jaintapauljp <jainta@openrefactory.com>
2023-05-19 10:23:06 +0600

    Fix Prevent SQL Injection
    In file: src.go, there is a method loginHandler that is vulnerable to <hyperlink1>. According to <hyperlink2>, without sufficient removal or quoting of SQL syntax in user-controllable inputs, the generated SQL query can cause those inputs to be interpreted as SQL. iCR identified the SQL injection sink where data has flowed without being validated properly.

--- ft/sql/03-gosqljson-query-to-array/src.go
+++ ft/sql/03-gosqljson-query-to-array/src.go
@@ -23,6 +23,21 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 	query := fmt.Sprintf("SELECT * FROM users WHERE username='%s' AND password='%s'", username, password)
 
+	// OpenRefactory Warning:
+	// Possible SQL injection!
+	// Path:
+	//	File: src.go, Line: 22
+	//		username := r.FormValue("username")
+	//		Variable 'username' is assigned a tainted value from an external source.
+	//	File: src.go, Line: 23
+	//		password := r.FormValue("password")
+	//		Variable 'password' is assigned a tainted value from an external source.
+	//	File: src.go, Line: 24
+	//		query := fmt.Sprintf("SELECT * FROM users WHERE username='%s' AND password='%s'", username, password)
+	//		Variable 'query' is assigned a tainted value which is passed through a function call.
+	//	File: src.go, Line: 26
+	//		gosqljson.QueryToArrays(db, gosqljson.AsIs, query)
+	//		Tainted information is used in a sink.
 	gosqljson.QueryToArrays(db, gosqljson.AsIs, query)
 }
 
