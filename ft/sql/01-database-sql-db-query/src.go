0f98a7d31c04f4e2e546a3152a917775d12aec04
sanjayor <sanjay@openrefactory.com>
2023-05-20 00:49:07 +0600

    Fix Prevent SQL Injection
    In file: src.go, there is a method loginHandler that is vulnerable to <hyperlink1>. According to <hyperlink2>, without sufficient removal or quoting of SQL syntax in user-controllable inputs, the generated SQL query can cause those inputs to be interpreted as SQL. iCR identified the SQL injection sink where data has flowed without being validated properly.

--- ft/sql/01-database-sql-db-query/src.go
+++ ft/sql/01-database-sql-db-query/src.go
@@ -19,6 +19,21 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 	query := fmt.Sprintf("SELECT * FROM users WHERE username=%s AND password=%s", username, password)
+	// OpenRefactory Warning:
+	// Possible SQL injection!
+	// Path:
+	//	File: src.go, Line: 19
+	//		username := r.FormValue("username")
+	//		Variable 'username' is assigned a tainted value from an external source.
+	//	File: src.go, Line: 20
+	//		password := r.FormValue("password")
+	//		Variable 'password' is assigned a tainted value from an external source.
+	//	File: src.go, Line: 21
+	//		query := fmt.Sprintf("SELECT * FROM users WHERE username=%s AND password=%s", username, password)
+	//		Variable 'query' is assigned a tainted value which is passed through a function call.
+	//	File: src.go, Line: 22
+	//		db.Query(query)
+	//		Tainted information is used in a sink.
 	db.Query(query)
 }
 
